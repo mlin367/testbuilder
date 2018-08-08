9ea3e86fe0a989f464c62187f358b05470bbd134
Matthew Lin <mlin367@yahoo.com>
2018-08-08 15:17:17 -0700

    Fill in the detectNetwork function
    Code is inputted into the empty detectNetwork function in order to make it fully
    functional.

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,13 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+  let cardLength = cardNumber.length;
+  let prefix = cardNumber.slice(0, 2);
+  if((prefix === '38' || prefix === '39') && cardLength === 14){
+  	return "Diner's Club";
+  }else if((prefix === '34' || prefix === '37') && cardLength === 15){
+  	return "American Express";
+  }
 };
 
 
