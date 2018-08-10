4db89d0bad72bd84b52500b80d43198d864abfee
Matthew Lin <mlin367@yahoo.com>
2018-08-10 13:53:46 -0700

    Extend function to include C UnionPay and Switch
    Although uniformity is compromised, both regex and the slice method are used for the two new
    credit card networks to make the function more concise.

--- detectNetwork.js
+++ detectNetwork.js
@@ -14,20 +14,25 @@ var detectNetwork = function(cardNumber) {
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
   let cardLength = cardNumber.length;
-  let prefix = cardNumber.slice(0, 2);
   let discover = RegExp('^(6011|64[4-9]|65)');
   let maestro = RegExp('^(5018|5020|5038|6304)');
-  if((prefix === '38' || prefix === '39') && cardLength === 14){
+  let chinaUnion = RegExp('^(62[4-6]|628[2-8])');
+  let switchCard = RegExp('^(4903|4905|4911|4936|564182|633110|6333|6759)');
+  if((cardNumber.slice(0, 2) === '38' || cardNumber.slice(0, 2) === '39') && cardLength === 14){
   	return 'Diner\'s Club';
-  }else if((prefix === '34' || prefix === '37') && cardLength === 15){
+  }else if((cardNumber.slice(0, 2) === '34' || cardNumber.slice(0, 2) === '37') && cardLength === 15){
   	return 'American Express';
-  }else if((prefix === '51' || prefix === '52' || prefix === '53' || prefix === '54' || prefix === '55') && cardLength === 16){
+  }else if((Number(cardNumber.slice(0, 6)) >= 622126 && Number(cardNumber.slice(0, 6)) <= 622925 || chinaUnion.test(cardNumber)) && (cardLength >= 16 && cardLength <= 19)){
+  	return 'China UnionPay';
+  }else if(switchCard.test(cardNumber) && (cardLength === 16 || cardLength === 18 || cardLength === 19)){
+  	return 'Switch';
+  }else if(Number(cardNumber.slice(0, 2)) >= 51 && Number(cardNumber.slice(0, 2)) <= 55  && cardLength === 16){
   	return 'MasterCard';
-  }else if(prefix[0] === '4' && (cardLength === 13 || cardLength === 16 || cardLength === 19)){
+  }else if(cardNumber.slice(0, 1) === '4' && (cardLength === 13 || cardLength === 16 || cardLength === 19)){
   	return 'Visa';
   }else if(discover.test(cardNumber) && (cardLength === 16 || cardLength === 19)){
     return 'Discover';
-  }else if(maestro.test(cardNumber) && (cardLength === 12 || cardLength === 13 || cardLength === 14 || cardLength === 15 || cardLength === 16 || cardLength === 17 || cardLength === 18 || cardLength === 19)){
+  }else if(maestro.test(cardNumber) && (cardLength >= 12 && cardLength <= 19)){
     return 'Maestro';
   }
 };
